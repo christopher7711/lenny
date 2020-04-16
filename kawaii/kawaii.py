de367aa959b15e1faf915c2920de0217802fe6b1
Christopher Hirtler <christopher.hirtler@student.tugraz.at>
2020-04-16 14:16:04 +0200

    fix2

--- kawaii/kawaii.py
+++ kawaii/kawaii.py
@@ -9,23 +9,23 @@ Cog: Any = getattr(commands, "Cog", object)
 
 class Kawaii(Cog):
     # post random file in specified folder in discord channel
-
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=934735897643)
         default_global = {
-            "kawaiipath": "None"
+            "kawaiipath": None
         }
         default_guild = {
-            "kawaiipath": "None"
+            "kawaiipath": None
         }
         self.config.register_global(**default_global)
         self.config.register_guild(**default_guild)
 
     @commands.command(aliases=["kawaiiii"])
     async def kawaii(self, ctx: commands.Context):
-        path = await self.config.guild(ctx.guild).kawaiipath()
-        if path != "None":
+        config = Config.get_conf(self, identifier=934735897643)
+        path = await config.guild(ctx.guild).kawaiipath()
+        if path != None:
             try:
                 await ctx.trigger_typing()
                 file = random.choice(os.listdir(path))
@@ -40,5 +40,6 @@ class Kawaii(Cog):
     @commands.command()
     @checks.is_owner()
     async def setkawaiipath(self, ctx, new_value):
-        await self.config.guild(ctx.guild).kawaiipath.set(new_value)
+        config = Config.get_conf(self, identifier=934735897643)
+        await config.guild(ctx.guild).kawaiipath.set(new_value)
         await ctx.send("Path has been set")
